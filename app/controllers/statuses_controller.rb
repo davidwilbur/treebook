21591644ab69eba2d8938cc3b2c88af72902b618
David Wilbur <davidwilbur1@gmail.com>
2014-05-08 11:34:54 -0400

    Updated frontend for the new authentication system
    Added simple_form
    Updated several forms to bootstrap/simple_form
    Added login/logout/register/profile links to our nav
    Added a collection select to our status form

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -67,8 +67,9 @@ class StatusesController < ApplicationController
       @status = Status.find(params[:id])
     end
 
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def status_params
-      params.require(:status).permit(:name, :content)
+      params.require(:status).permit(:user_id, :content)
     end
 end
