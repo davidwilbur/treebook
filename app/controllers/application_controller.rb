dc8f141484d11cc4b0a9a6676eb85cdfa763c149
David Wilbur <davidwilbur1@gmail.com>
2014-05-10 17:55:35 -0400

    Added user friendships and associated tests.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,6 @@ class ApplicationController < ActionController::Base
   	devise_parameter_sanitizer.for(:sign_up) << :last_name
   	devise_parameter_sanitizer.for(:sign_up) << :profile_name
   end
+
+
 end
